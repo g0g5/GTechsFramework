d0b6bbe1db550de53d7c930dd0143ef2b54bff1b
g0g5 <graphicguy2025@163.com>
2023-07-04 21:32:23 +0800

    character module

---
+++ Character/Scripts/Component/Character.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GTechs.Character
+{
+    public partial class CharacterData { }
+
+    public class Character : MonoBehaviour
+    {
+
+        public CharacterData data;
+
+        private CharacterController m_controller;
+
+        void Awake()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
